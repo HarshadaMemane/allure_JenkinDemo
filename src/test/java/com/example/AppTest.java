43f4ac346edca5790cff9c988f3ed5bf9fd244f2
HarshadaMemane <146841610+HarshadaMemane@users.noreply.github.com>
2025-09-24 12:59:58 +0530

    Update AppTest.java
    code committed by Harshada

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -9,20 +9,21 @@ import io.qameta.allure.Description;
 public class AppTest {
 
     @Test
-    @Description("Simple test to check substractiob")
-    public void testsubstraction() {
-        int a = 20;
-        int b = 5;
-        int diff = a - b;
-        assertEquals(diff, 15, "diff should be 15");
-    }
-    @Test
     @Description("Simple test to check Addition")
     public void testsubstraction() {
         int a = 10;
         int b = 5;
         int sum = a + b;
         assertEquals(sum, 15, "Sum should be 15");
+    }
+
+    @Test
+    @Description("Simple test to check substractiob")
+    public void testsubstraction() {
+        int a = 20;
+        int b = 5;
+        int diff = a - b;
+        assertEquals(diff, 15, "diff should be 15");
     }
     @Test
     @Description("Simple test to check multiplication")
