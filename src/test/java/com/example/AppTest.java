294ab8a5ceb1692b154c39e39c3145fb97720ed5
HarshadaMemane <146841610+HarshadaMemane@users.noreply.github.com>
2025-09-24 12:49:18 +0530

    Update AppTest.java
    code committed by harshada

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -14,7 +14,23 @@ public class AppTest {
         int a = 20;
         int b = 5;
         int diff = a - b;
-        assertEquals(diff, 15, "Sum should be 15");
+        assertEquals(diff, 15, "diff should be 15");
+    }
+    @Test
+    @Description("Simple test to check Addition")
+    public void testsubstraction() {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        assertEquals(sum, 15, "Sum should be 15");
+    }
+    @Test
+    @Description("Simple test to check multiplication")
+    public void testmultiply() {
+        int a = 10;
+        int b = 5;
+        int mul = a * b;
+        assertEquals(mul, 50, "mul should be 15");
     }
 }
 
